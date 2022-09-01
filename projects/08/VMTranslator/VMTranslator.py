e163db0f6316b245ff19c03fa0ff1743a2ab37ab
Tom OMara <toma0001@student.monash.edu>
2022-09-01 13:21:20 +1000

    fixing simple function

--- projects/08/VMTranslator/VMTranslator.py
+++ projects/08/VMTranslator/VMTranslator.py
@@ -1,24 +1,15 @@
 import sys
 import os
-#read_file_name = sys.argv[1]
-#print(os.path.basename(read_file_name))
-#read_file_name = sys.argv[1]
 
-read_file_name = os.path.basename(sys.argv[1])
-#print(read_file_name)
-
+read_file_full_path = sys.argv[1]
+read_file_name = os.path.basename(read_file_full_path)
 base_file_name = read_file_name[0:read_file_name.index('.')]
-write_file_name = base_file_name + ".asm"
-#print(base_file_name)
 
-'''
-base_file_name = "StaticTest"
-read_file_name = "MemoryAccess\StaticTest\StaticTest.vm"
-write_file_name = "MemoryAccess\StaticTest\StaticTest.asm"
-'''
+write_file_full_path = read_file_full_path[0:read_file_full_path.index('.')] + ".asm"
 
-jump_count = 0 #global jump_count
-function_name = "main"
+jump_count = 0          # Global jump_count for jump labels
+function_name = "main"  # Track function names for function labels
+local_var_count = 0     # Track number of local variables for active function
 
 alc_double_code = {
     "add" : "D+M", 
@@ -44,7 +35,6 @@ comparison_code = {
 }
 
 memory_location = {
-    "pointer"  : "SP",
     "local"    : "LCL",
     "argument" : "ARG",
     "this"     : "THIS",
@@ -63,8 +53,6 @@ def parser(line):
             return commands_list[0], commands_list[1], commands_list[2]
 
 def code_writer(command, arg1, arg2):
-    # Process Arithmetic Logical Commands
-    
     if command == "push":
         return generate_push_string(arg1, arg2)
 
@@ -73,9 +61,18 @@ def code_writer(command, arg1, arg2):
 
     elif command == "label":
             return generate_label_string(arg1)
+    
+    elif command == "function":
+            return generate_function_string(arg1, arg2)
 
     elif command == "if-goto":
             return generate_ifgoto_string(arg1)
+
+    elif command == "goto":
+            return generate_goto_string(arg1)
+
+    elif command == "return":
+            return generate_return_string(arg2)
 
     elif command in comparison_code:
         alc_code = comparison_code[command]
@@ -192,6 +189,34 @@ def calculate_memory_location(arg1, arg2):
     
     return assembly_string
 
+def restore_values(memory_name):
+    assembly_string = ""
+    if memory_name == "THAT":   offest = '1' 
+    if memory_name == "THIS":   offest = '2'
+    if memory_name == "ARG":    offest = '3'
+    if memory_name == "LCL":    offest = '4'
+    
+    # Store value of offset in D
+    assembly_string += "@" + offest + '\n'
+    assembly_string += "D=A" + '\n'
+
+    # Get value of LCL and deduct offset
+    assembly_string += "@R13" + '\n'
+    assembly_string += "A=M" + '\n'
+    assembly_string += "D=M-D" + '\n'
+
+    # Store value at memory location D into D
+    assembly_string += "A=D" + '\n'
+    assembly_string += "A=M" + '\n'
+    assembly_string += "D=M" + '\n'
+
+    # Store value of D into base memory
+    assembly_string += "@" + memory_name + '\n'
+    assembly_string += "M=D" + '\n'
+
+    return assembly_string
+
+
 def generate_push_string(arg1, arg2):
     assembly_string = ""
     if arg1 == "constant":
@@ -230,6 +255,67 @@ def generate_label_string(arg1):
     assembly_string += '(' + convert_label_name(arg1) + ')' + '\n'
     return assembly_string
 
+def generate_function_string(arg1, arg2):
+    # When a function line is executed eg "function SimpleFunction.test 2"
+    # Labels function line and sets n local variables to zero
+    assembly_string = ""
+    assembly_string += '(' + arg1 + ')' + '\n'
+    local_var_count = int(arg2)
+    for i in range(local_var_count):
+        assembly_string += generate_push_string("constant", "0")
+        assembly_string += generate_pop_string("local", str(i))
+        assembly_string += move_sp_forward()
+    return assembly_string
+
+def generate_return_string(num_args):
+    assembly_string = ""
+    # 1. Save LCL into R13
+    assembly_string += "@ARG" + '\n'
+    assembly_string += "A=M" + '\n'
+    assembly_string += "D=M" + '\n'
+    assembly_string += "@R13" + '\n'
+    assembly_string += "M=D" + '\n'
+    # 2. Save return address to R14 (LCL - 5)
+    assembly_string += "@5" + '\n'     # Calc where in memory the ret address is
+    assembly_string += "D=A" + '\n'
+    assembly_string += "@LCL" + '\n'
+    assembly_string += "A=M" + '\n'
+    assembly_string += "D=M-D" + '\n'  # D = memory location of ret address 
+    
+    assembly_string += "A=D" + '\n'     # Goto return address location and save value
+    assembly_string += "A=M" + '\n'
+    assembly_string += "D=M" + '\n'    # D = value of return address
+
+    assembly_string += "@R13" + '\n'    # Save value of return address in R13
+    assembly_string += "A=M" + '\n'
+    assembly_string += "M=D" + '\n'
+
+    # 3. Place return value back to return address
+    assembly_string += move_sp_back() # Save return value into D
+    assembly_string += set_m_to_sp()
+    assembly_string += "D=M" + '\n'
+    assembly_string += "D=A" + '\n'
+
+    assembly_string += "@ARG" + '\n'
+    assembly_string += "A=M" + '\n'
+    assembly_string += "M=D" + '\n'
+
+    # 4. Reset stack pointer
+    assembly_string += "@ARG" + '\n'
+    assembly_string += "A=M" + '\n'
+    assembly_string += "D=M" + '\n'
+    assembly_string += "@SP" + '\n'
+    assembly_string += "M=D" + '\n'
+    assembly_string += move_sp_forward()
+
+    # Restore THAT, THIS, LOCAL
+    assembly_string += restore_values("THAT")
+    assembly_string += restore_values("THIS")
+    assembly_string += restore_values("ARG")
+    assembly_string += restore_values("LCL")
+    
+    return assembly_string
+
 def generate_ifgoto_string(arg1):
     assembly_string = ""
     label_name = convert_label_name(arg1)
@@ -238,13 +324,29 @@ def generate_ifgoto_string(arg1):
     assembly_string += "D; JNE" + '\n'
     return assembly_string
 
+def generate_goto_string(arg1):
+    assembly_string = ""
+    label_name = convert_label_name(arg1)
+    assembly_string += "@" + label_name + '\n'
+    assembly_string += "0; JMP" + '\n'
+    return assembly_string
+
+'''
+def create_function_name(function_name):
+    # Converts a function name for labelling
+    # Function name eg "myFunc" in file "fileName" -> fileName.myFunc
+    label_string = ""
+    label_string += base_file_name + '.' + function_name
+    return label_string
+'''
+
 def convert_label_name(label_name):
     label_string = ""
     label_string += base_file_name + '.' + function_name + '$' + label_name
     return label_string
 
-with open(read_file_name) as read_file:
-    write_file = open(write_file_name, "w")
+with open(read_file_full_path) as read_file:
+    write_file = open(write_file_full_path, "w")
     for line in read_file:
         if line != '\n' and line[0] != '/':
             assembly_string = ""
